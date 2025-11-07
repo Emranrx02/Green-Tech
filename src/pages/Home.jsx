fa901ac7c2008a36000b86df57a4f055abe80fb6
Emran Haque <emran.huk2016@gmail.com>
2025-11-07 15:40:02 +0600

    fix(assets): replace missing Hasibur.jpg with antor.jpg to avoid build error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ import nurnanbi from "../assets/nurnabi.jpeg";
 import samiul from "../assets/samiul.jpeg";
 import female2 from "../assets/female2.jpeg";
 import miah from "../assets/miah.jpeg";
-import Hasibur from "../assets/Hasibur.jpg";
+import antor from "../assets/antor.jpg";
 
 
 export default function Home() {
@@ -148,7 +148,7 @@ export default function Home() {
   { name: "Md.Samiul Alam", role: "Junior custom relations executive\nSenior Marketing Executive", img: samiul, icons: ["telegram"] },
   { name: "Raisa Riaz", role: "Senior custom relations Executive & trainer", img: female2, icons: ["telegram"] },
   { name: "Miah Mohammad Tamjid", role: "Web Developer", img: miah, icons: ["telegram"] },
-  { name: "Hasibur Rahman", role: "Senior custom relations Executive & trainer", img: Hasibur, icons: ["telegram"] }
+  { name: "Hasibur Rahman", role: "Senior custom relations Executive & trainer", img: antor, icons: ["telegram"] }
 ].map((p, i) => (
               <Reveal className="personReveal" key={i} once={true} rootMargin="0px 0px -10% 0px">
                 <article className="person">
