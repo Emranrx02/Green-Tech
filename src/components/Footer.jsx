ca8c321445613fb4a5efa4cbf65a0d38843ef846
Emran Haque <emran.huk2016@gmail.com>
2025-12-10 02:33:32 +0600

    Footer: left-align copyright, add AI badge right, responsive stack on mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,52 @@
 import React from "react";
-import logo from "../assets/logo.jpg";
 import { FaMapMarkerAlt, FaEnvelope, FaPhone } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="container footerGrid">
+
+        {/* LEFT – Location */}
         <div className="footerCol footerLeft">
+          <h4 className="footerTitle">Location</h4>
           <div className="locRow">
             <FaMapMarkerAlt className="locIcon" />
-            <div>
-              <div className="locTitle">Location</div>
-              <div className="locText">Azampur, Uttara</div>
-            </div>
+            <div className="locText">Azampur, Uttara</div>
           </div>
         </div>
 
+        {/* CENTER – Useful Links */}
         <div className="footerCol footerCenter">
-          <div className="linksCol">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/services">Services</a></li>
-              <li><a href="/contact">Contact</a></li>
-            </ul>
-          </div>
-
-          <div className="contactCol">
-            <div className="contactItem"><FaEnvelope className="contactIcon" /><a href="mailto:info@grentechit.com">info@grentechit.com</a></div>
-            <div className="contactItem"><FaPhone className="contactIcon" /><a href="tel:+8801619012356">01619012356</a></div>
-          </div>
+          <h4 className="footerTitle">Useful Links</h4>
+          <ul className="linksList">
+            <li><a href="/">Home</a></li>
+            <li><a href="/services">Services</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
         </div>
 
+        {/* RIGHT – Contact (replacing logo) */}
         <div className="footerCol footerRight">
-          <div className="brandRow brandBox">
-            {logo ? <img src={logo} alt="Green Tech" className="brandLogo" /> : null}
-            <div className="brandInfo">
-              <div className="brandName">Green Tech</div>
-              <div className="brandTag">A platform to prove your skill</div>
-            </div>
+          <h4 className="footerTitle">Contact</h4>
+          <div className="contactItem">
+            <FaEnvelope className="contactIcon" />
+            <a href="mailto:info@grentechit.com">info@grentechit.com</a>
+          </div>
+          <div className="contactItem">
+            <FaPhone className="contactIcon" />
+            <a href="tel:+8801619012356">01619012356</a>
           </div>
         </div>
+
       </div>
 
-      <div className="container footerBottom footerCenter">
-        <span className="copyright">© {new Date().getFullYear()} Green Tech • Developed by <a href="https://emranhaque.com" target="_blank" rel="noopener noreferrer">Emran Haque</a>.</span>
+      {/* Bottom: left copyright, right small AI badge (keeps chat/widget area clear) */}
+      <div className="container footerBottom">
+        <div className="footerBottomLeft">
+          <span className="copyright">© {new Date().getFullYear()} Green Tech • Developed by <a href="https://emranhaque.com" target="_blank" rel="noopener noreferrer">Emran Haque</a>.</span>
+        </div>
+
+        
       </div>
     </footer>
   );
